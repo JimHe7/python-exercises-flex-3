c299e9fae05435b2324a9d325f58cecf85a7f457
Jim He <jimhe7@gmail.com>
2018-03-18 22:18:25 +0000

    io ex final

--- python-exercises-flex-3/ioExercises.py
+++ python-exercises-flex-3/ioExercises.py
@@ -6,9 +6,22 @@ with open(fileName, 'r') as file_handle:
   contents = file_handle.read()
 print(contents)
 
-"""
+
 #EX2
 fileName = input("File Name?")
 with open(fileName, 'w') as file_handle:
   contents = input("Contents of file?")
   file_handle.write(contents)
+
+"""
+
+#EX3
+import sys
+sys.path.append("dictionary.py")
+
+from dictionary import word_histogram
+fileName = input("File Name?")
+with open(fileName, 'r') as file_handle:
+  contents = file_handle.read()
+
+print(word_histogram(contents))
