05a015829caaea2597f9be5c7965f31cd2c73cfb
Jim He <jimhe7@gmail.com>
2018-03-18 19:49:36 +0000

    intermediate

--- python-exercises-flex-3/ioExercises.py
+++ python-exercises-flex-3/ioExercises.py
@@ -1,4 +1,14 @@
+#EX1
+
+"""
 fileName = input("File Name?")
 with open(fileName, 'r') as file_handle:
   contents = file_handle.read()
 print(contents)
+
+"""
+#EX2
+fileName = input("File Name?")
+with open(fileName, 'w') as file_handle:
+  contents = input("Contents of file?")
+  file_handle.write(contents)
