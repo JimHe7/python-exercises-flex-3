bf85491a46fb15767ef88d5f209a51dd7c7b1576
Jim He <jimhe7@gmail.com>
2018-03-18 22:20:18 +0000

    io ex final

--- python-exercises-flex-3/ioExercises.py
+++ python-exercises-flex-3/ioExercises.py
@@ -1,6 +1,6 @@
 #EX1
 
-"""
+
 fileName = input("File Name?")
 with open(fileName, 'r') as file_handle:
   contents = file_handle.read()
@@ -13,7 +13,7 @@ with open(fileName, 'w') as file_handle:
   contents = input("Contents of file?")
   file_handle.write(contents)
 
-"""
+
 
 #EX3
 import sys
